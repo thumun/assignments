077e683f27a2adbafdceca0abe05069974822ceb
nehathumu <nthumu@haverford.edu>
2022-01-15 16:46:51 -0500

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,14 @@
+//
+// Created by Neha Thumu on 1/15/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("A day like any other. A routine that the body and mind are accustomed to.\n"
+           "An item will appear to replace something you lost, which will bring you great happiness.\n"
+           "Relations with the acquaintances you see most frequently will improve, and some may even become the best of friends.\n"
+           "Even the most ordinary of days can become a treasured memory.\n");
+    printf("\nQuote from Genshin Impact");
+    return 0;
+}
