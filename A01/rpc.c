5b298539b25c1df0bd42df10860972b312ca1ed3
nehathumu <nthumu@haverford.edu>
2022-01-28 12:29:33 -0500

    added comments & fixed rpc

--- A01/rpc.c
+++ A01/rpc.c
@@ -3,96 +3,87 @@
 #include <time.h>
 #include <string.h>
 
-//void pick_winner(char * user_choice, int AIPick, const char * game_options[]){
-//    int user;
-//    int winner;
-//
-//    if (strcmp(user_choice, game_options[0]) == 0){
-//        user = 0;
-//    } else if (strcmp(user_choice, game_options[1]) == 0) {
-//        user = 1;
-//    } else if (strcmp(user_choice, game_options[2]) == 0) {
-//        user = 2;
-//    }
-//
-//    if (AIPick > user){
-//
-//
-//    } else if (AIPick == user){
-//
-//
-//    } else {
-//
-//    }
-//
-//    printf("%s bashes %s\n", user_choice, AI_choice);
-//
-//}
+// returns 1 if user or AI wins, 0 if it's a draw or a loss
+// also sets message depending on rps choice and outcome
+int pickWinner(const char * first, const char * second, char * msg){
+    if (strcmp(first, second) == 0){
+        strcpy(msg, "It's a draw!\n");
+        return 0;
+    } else {
+        if (((strcmp(second, "rock") == 0) && (strcmp(first, "scissors") == 0))){
+            strcpy(msg, "rock bashes scissors\n");
+            return 1;
+        } else if (((strcmp(second, "paper") == 0) && (strcmp(first, "rock") == 0))){
+            strcpy(msg, "paper covers rock\n");
+            return 1;
+        } else if (((strcmp(second, "scissors") == 0) && (strcmp(first, "paper") == 0))){
+            strcpy(msg, "scissors cuts paper\n");
+            return 1;
+        }
 
-int AI_pick(){
-    int random_variable = rand();
-    return random_variable % 3;
+        return 0;
+    }
+
+}
+
+// AI choice for rps game
+int AIPick(){
+    return rand() % 3;
 }
 
 void game(int rounds){
-    char user_choice[100];
-    const char * AI_choice;
-    int AIPick;
-    int user_score = 0;
-    int AI_score = 0;
-    int winner;
+    char userChoice[100];
+    const char * AIChoice;
+    int userScore = 0;
+    int AIScore = 0;
+    char msg[100];
 
-    // * = pointer to a string
-    // is array size 3; each type is char * (string)
-    const char * game_options[3] = {"rock", "paper", "scissors"};
+    // array of strings for game options
+    const char * gameOptions[3] = {"rock", "paper", "scissors"};
 
     while (rounds != 0) {
 
-        printf("Which do you choose? rock, paper, or scissors? ");
-        scanf("%s", user_choice);
+        printf("\nWhich do you choose? rock, paper, or scissors? ");
+        scanf("%s", userChoice);
 
-        // using random num to get AI's choice
-        AIPick = AI_pick();
-        AI_choice = game_options[AIPick];
-
-        printf("AI chose %s\n", AI_choice);
-
-        //winner = pick_winner(user_choice, AI_score, game_options);
-
-        if (((strcmp(user_choice, "rock") == 0) && (strcmp(AI_choice, "scissors") == 0))){
-            printf("%s bashes %s\n", user_choice, AI_choice);
-            user_score += 1;
-        } else if (((strcmp(user_choice, "paper") == 0) && (strcmp(AI_choice, "rock") == 0))){
-            printf("%s covers %s\n", user_choice, AI_choice);
-            user_score += 1;
+        // quit condition
+        if (strcmp(userChoice, "quit") == 0){
+            printf("Quitting the game...");
+            return;
         }
-        else if (((strcmp(user_choice, "scissors") == 0) && (strcmp(AI_choice, "paper") == 0))){
-            printf("%s cuts %s\n", user_choice, AI_choice);
-            user_score += 1;
-        } else if (strcmp(user_choice, AI_choice) == 0) {
-            printf("It's a draw!\n");
-        } else if (strcmp(user_choice, "rock") != 0
-                   && strcmp(user_choice, "paper") != 0
-                   && strcmp(user_choice, "scissors") != 0) {
 
-            printf("You entered an invalid choice: %s\n", user_choice);
-            printf("%lu", strlen(user_choice));
+        AIChoice = gameOptions[AIPick()];
+
+        printf("AI chose %s\n", AIChoice);
+
+        // invalid choice condition
+        if (strcmp(userChoice, "rock") != 0
+                   && strcmp(userChoice, "paper") != 0
+                   && strcmp(userChoice, "scissors") != 0) {
+
+            printf("You entered an invalid choice: %s\n", userChoice);
+            //printf("%lu", strlen(userChoice));
         } else {
-            if (((strcmp(user_choice, "rock") == 0) && (strcmp(AI_choice, "scissors") == 0))){
-                printf("%s bashes %s\n", AI_choice, user_choice);
-            } else if (((strcmp(user_choice, "paper") == 0) && (strcmp(AI_choice, "rock") == 0))){
-                printf("%s covers %s\n", AI_choice, user_choice);
-            }
-            else if (((strcmp(user_choice, "scissors") == 0) && (strcmp(AI_choice, "paper") == 0))){
-                printf("%s cuts %s\n", AI_choice, user_choice);
-            }
+            // adding to the AI or user score depending on who wins rps
+            AIScore += pickWinner(userChoice, AIChoice, msg);
+            userScore += pickWinner(AIChoice, userChoice, msg);
 
-            AI_score += 1;
+            printf("%s", msg);
         }
 
-        printf("AI score: %d, Player score: %d\n", AI_score, user_score);
+        printf("AI score: %d, Player score: %d\n", AIScore, userScore);
 
         rounds -= 1;
+
+    }
+
+    // declares who wins at end of rounds
+    if (userScore > AIScore){
+        printf("Player wins!");
+    } else if (userScore == AIScore){
+        printf("It's a draw!");
+    } else{
+        printf("AI wins :(");
     }
 }
 
@@ -101,8 +92,10 @@ int main() {
   int rounds;
 
   printf("Welcome to Rock, Paper, Scissors!\n");
-  printf("How many rounds do you want to play?\n");
+  printf("How many rounds do you want to play? ");
   scanf("%d", &rounds);
+
+  // printf("you have chosen %d rounds", rounds);
 
   game(rounds);
 
--- A01/snackbar.c
+++ A01/snackbar.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// global variables
-int money;
-
 // struct contains necessary info about snacks
 struct snack {
     char name[100];
@@ -12,61 +9,64 @@ struct snack {
 };
 
 // diff. outcomes when buying a snack
-void buy_snack(struct snack item, float  money){
-	if (item.quantity == 0){ 
-		printf("Sorry, %s is out of stock!", item.name);
-	} else if (item.cost > money){
-		printf("Sorry, you do not have enough money to purchase %s", item.name);
-	} else {
+void buy_snack(struct snack item, float money){
+    if (item.quantity == 0){
+        printf("Sorry, %s is out of stock!", item.name);
+    } else if (item.cost > money){
+        printf("Sorry, you do not have enough money to purchase %s", item.name);
+    } else {
         printf("Thank you for purchasing %s!\n", item.name);
         printf("Your current balance is: $%0.3f", money - item.cost);
     }
-} 
+}
 
 int main() {
-  int option;
+    int option;
+    int money;
 
-  struct snack gummy, choco, gum;   
+    struct snack gummy, choco, gum;
 
-  strcpy(gummy.name, "Gummy Worms"); 
-  gummy.cost = 3.25;  
-  gummy.quantity = 10; 
+    strcpy(gummy.name, "Gummy Worms");
+    gummy.cost = 3.25;
+    gummy.quantity = 10;
 
-  strcpy(choco.name, "Chocolate"); 
-  choco.cost = 5.75; 
-  choco.quantity = 0; 
+    strcpy(choco.name, "Chocolate");
+    choco.cost = 5.75;
+    choco.quantity = 0;
 
-  strcpy(gum.name, "Bubble gum"); 
-  gum.cost = 0.75; 
-  gum.quantity = 3; 
+    strcpy(gum.name, "Bubble gum");
+    gum.cost = 0.75;
+    gum.quantity = 3;
 
-  struct snack options[3];
-  
-  options[0] = gummy; 
-  options[1] = choco; 
-  options[2] = gum;  
+    struct snack options[3];
 
-  printf("Welcome to Neha's Fantastic Snacks.\n\n");
-  printf("How much money do you have?\n\n"); 
+    options[0] = gummy;
+    options[1] = choco;
+    options[2] = gum;
 
-  scanf("%d", &money);
+    printf("Welcome to Neha's Fantastic Snacks.\n\n");
+    printf("How much money do you have? ");
 
-  printf("0) %s \t\t cost: $%0.2f \t quantity: %d\n", gummy.name, gummy.cost, gummy.quantity);
-  printf("1) %s \t\t cost: $%0.2f \t quantity: %d\n", choco.name, choco.cost, choco.quantity);
-  printf("2) %s \t\t cost: $%0.2f \t quantity: %d\n", gum.name, gum.cost, gum.quantity);
+    scanf("%d", &money);
 
-  printf("\nWhat snack would you like to buy? [0, 1, 2] ");
-  scanf("%d", &option);
+    printf("\n");
 
-  if (option == 0){
-	buy_snack(options[0], money);
-  } else if (option == 1){
-	buy_snack(options[1], money);
-  } else if (option == 2){
-	buy_snack(options[2], money);
-  } else {
-	printf("Snack not available"); 
-  } 
+    printf("0) %s \t\t cost: $%0.2f \t quantity: %d\n", gummy.name, gummy.cost, gummy.quantity);
+    printf("1) %s \t\t cost: $%0.2f \t quantity: %d\n", choco.name, choco.cost, choco.quantity);
+    printf("2) %s \t\t cost: $%0.2f \t quantity: %d\n", gum.name, gum.cost, gum.quantity);
 
-  return 0;
+    printf("\nWhat snack would you like to buy? [0, 1, 2] ");
+    scanf("%d", &option);
+
+    if (option == 0){
+        buy_snack(options[0], money);
+    } else if (option == 1){
+        buy_snack(options[1], money);
+    } else if (option == 2){
+        buy_snack(options[2], money);
+    } else {
+        printf("Snack not available");
+    }
+
+    return 0;
 }
