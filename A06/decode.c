a002c70ac74a127beb81a8556793de5d6daa9f43
nehathumu <nthumu@haverford.edu>
2022-03-01 22:42:16 -0500

    first part done

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
 
+// getting the least significant bit of each rgb val via modulus 2
+void getBits(struct ppm_pixel * arrPx, int w, int h, char * numChar){
+
+    for (int i = 0; i < w*h; i++){
+        if (arrPx[i].red % 2 == 0){
+            numChar[i*3] = '0';
+        } else {
+            numChar[i*3] = '1';
+        }
+
+        if (arrPx[i].green % 2 == 0){
+            numChar[i*3+1] = '0';
+        } else {
+            numChar[i*3+1] = '1';
+        }
+
+        if (arrPx[i].blue % 2 == 0){
+            numChar[i*3+2] = '0';
+        } else {
+            numChar[i*3+2] = '1';
+        }
+    }
+
+    // making sure that length of string multiple of 8
+    // so ascii works properly
+
+    int remainder = w*h*3 % 8;
+
+    for(int i = 0; i < remainder; i++){
+        numChar[w*h*3 + i] = '0';
+    }
+    numChar[w*h*3+remainder] = '\0';
+}
+
 int main(int argc, char** argv) {
-  return 0;
+
+    char * filename;
+    int width;
+    int height;
+
+    struct ppm_pixel * arrPx;
+
+    // input checker - change
+    if (argc != 2){
+        printf("usage:");
+        for (int i = 0; i < argc; i++){
+            printf(" %s", argv[i]);
+        }
+        return 1;
+    }
+
+    filename = argv[1];
+
+    // reading data as array of pixels
+    arrPx = read_ppm(filename, &width, &height);
+
+    char * numChar = malloc(sizeof(char)*width*height*3 + 10);
+
+    getBits(arrPx, width, height, numChar);
+
+    // testing output
+    printf("Reading %s.ppm with width %d and height %d\n", filename, width, height);
+    printf("Max number of characters in the image: %lu\n", strlen(numChar)/8);
+
+    // logic for printing the message
+    for (int i = 0; i < strlen(numChar); i++){
+
+        // char is 8 bits
+        if (i%8 == 0){
+
+            // temp array to get the 8 bits that make up the char
+            char digits[9];
+            strncpy(digits, &numChar[i], 8);
+            digits[8] = '\0';
+
+            // as a note, found this handy function on stack overflow
+            char temp = strtol(digits, 0, 2);
+            printf("%c", temp);
+
+            if (temp == '\0'){
+                break;
+            }
+        }
+    }
+
+    // free-ing
+    free(arrPx);
+    free(numChar);
+    arrPx = NULL;
+    numChar = NULL;
+
+    return 0;
 }
 
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -8,7 +8,68 @@
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
 
-  return NULL;
+    FILE * file = NULL;
+    char line[1024];
+    char magicNum[3];
+
+    struct ppm_pixel * arrPx = NULL;
+
+    file = fopen(filename, "rb");
+
+    // file pointer test
+    if (file == NULL) {
+        printf("Cannot open file: %s", filename); // print error
+        return NULL;
+    }
+
+    while (strncmp(fgets(line, sizeof(line), file), "255", 3) != 0) {
+        // getting magic number
+        if (line[0] == 'P'){
+            strncpy(magicNum, line, 2);
+            // skipping lines with comments or blanks
+        } else if (line[0] == '#' || line[0] == ' '){
+            continue;
+        } else {
+            // getting & setting width & height
+            sscanf(line, "%d %d", w, h);
+
+        }
+    }
+
+    // if P3 then re-opening as not binary
+    if (strcmp(magicNum, "P3") == 0){
+        fclose(file);
+        fopen(filename, "r");
+
+        while (strncmp(fgets(line, sizeof(line), file), "255", 3) != 0) {
+            // getting magic number
+            if (line[0] == 'P'){
+                strncpy(magicNum, line, 2);
+                // skipping lines with comments or blanks
+            } else if (line[0] == '#' || line[0] == ' '){
+                continue;
+            } else {
+                // getting & setting width & height
+                sscanf(line, "%d %d", w, h);
+
+            }
+        }
+    }
+
+    arrPx = (struct ppm_pixel *) malloc(*h * *w * sizeof(struct ppm_pixel));
+
+    if (arrPx == NULL){
+        printf("malloc failed");
+        return NULL;
+    }
+
+    // reading stuff in file & putting them as ppm_pixel in the array
+    fread(arrPx, sizeof(struct ppm_pixel), *h * *w, file);
+
+    fclose(file);
+    file = NULL;
+
+    return arrPx;
 }
 
 // TODO: Implement this function
