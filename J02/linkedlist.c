5f8c410e53a0317f09cf7d78e829a4f7b100b6ff
nehathumu <nthumu@haverford.edu>
2022-03-03 15:09:06 -0500

    linked list done

--- J02/linkedlist.c
+++ J02/linkedlist.c
@@ -8,52 +8,39 @@ struct cake {
 };
 
 void printNodes(struct cake* list){
-
     struct cake* current = list;
 
     while(current != NULL){
 
-        printf("\n cake: %s ($%0.02f)", current->flavor, current->cost);
+        printf("\ncake: %s ($%0.02f)", current->flavor, current->cost);
         current = current->next;
 
     }
-
-//    struct cake* a = list;
-//
-//    while(a != NULL){
-//        if (costcheap < a->cost){
-//            costcheap = a->cost;
-//            strcpy(cake, a->flavor);
-//            printf("test cost: %f, %s\n", costcheap, cake);
-//        }
-//
-//        a = a->next;
-//
-//    }
 }
 
-char * findCheapest(struct cake* list){
+struct cake * findCheapest(struct cake* list){
     struct cake* current = list;
     struct cake* next = current->next;
+    struct cake* cheapest;
 
     float cost = list->cost;
 
-    char * cakeName = NULL;
-
+    //char * cakeName = NULL;
 
     //float * prices = (float *)malloc(sizeOf(float)*15);
 
     while(next!=NULL){
         if (cost > current->cost){
+            cheapest = current;
             cost = current->cost;
-            strcpy(cakeName, current->flavor);
+            //strcpy(cakeName, current->flavor);
         }
 
         current = next;
         next = next->next;
     }
 
-    return cakeName;
+    return cheapest;
 }
 
 struct cake makeCake(const char* flavor, float cost) {
@@ -67,24 +54,26 @@ struct cake makeCake(const char* flavor, float cost) {
 // todo: implement cheapestCake
 
 int main() {
-  struct cake cake1 = makeCake("red velvet", 2.00);
-  struct cake cake2 = makeCake("chocolate", 1.75);
-  struct cake cake3 = makeCake("mocha", 3.50);
-  // todo: define 4th cake
-  struct cake cake4 = makeCake("rainbow sprinkle", 5.25);
+    struct cake cake1 = makeCake("red velvet", 2.00);
+    struct cake cake2 = makeCake("chocolate", 1.75);
+    struct cake cake3 = makeCake("mocha", 3.50);
+    // todo: define 4th cake
+    struct cake cake4 = makeCake("rainbow sprinkle", 5.25);
 
-  cake1.next = &cake2;
-  cake2.next = &cake3;
+    cake1.next = &cake2;
+    cake2.next = &cake3;
 
-  cake3.next = &cake4;
-  // draw stack and heap here
+    cake3.next = &cake4;
+    // draw stack and heap here
 
     printNodes(&cake1);
 
-    char cakeName[32];
+    printf("\n");
 
-    strcpy(cakeName, findCheapest(&cake1));
-    printf("cheapest cake is: %s", cakeName);
+    //char cakeName[32];
+    //strcpy(cakeName, findCheapest(&cake1));
 
-  // todo: call cheapestCake
+    struct cake cheapest = *findCheapest(&cake1);
+
+    printf("cheapest cake is: %s", cheapest.flavor);
 }
