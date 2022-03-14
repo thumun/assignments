5bd48e827e1c19103e7fb184c42407d0c9769ef3
nehathumu <nthumu@haverford.edu>
2022-03-14 18:20:13 -0400

    fixing code

--- J02/readvector.c
+++ J02/readvector.c
@@ -6,42 +6,32 @@
 // The values should be returned in a flat float array
 float* readvector(const char* filename, int *size) {
     FILE * file = NULL;
+    file = fopen(filename,"r");
 
-    file = fopen(filename, "r");
-
-    float * vector;
-
-    // file pointer test
-    if (file == NULL) {
-        printf("Cannot open file: %s", filename); // print error
+    if(file == NULL){
+        printf("Cannot open file: %s",filename);
         exit(1);
     }
     char buff[100];
     fgets(buff,100,file);
     sscanf(buff,"%d",size);
-    vector = malloc(sizeof(size));
+    float *vector = malloc(sizeof(float) * (*size));
     for(int i = 0; i < *size;i++){
         fscanf(file,"%f",&vector[i]);
     }
-
     fclose(file);
     return vector;
 }
-
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("usage: readvector <filename>\n");
-    exit(0);
-  }
-
-  int size = 0;
-  float* vector = readvector(argv[1], &size);
-
-  for(int i = 0; i < size;i++){
-      printf("%f\n",vector[i]);
-  }
-
-  free(vector);
-  return 0;
+    if (argc != 2) {
+        printf("usage: readvector <filename>\n");
+        exit(0);
+    }
+    int size = 0;
+    float* vector = readvector(argv[1], &size);
+    for(int i = 0; i < size; i++){
+        printf("%f\n",vector[i]);
+    }
+    free(vector);
+    return 0;
 }
-
