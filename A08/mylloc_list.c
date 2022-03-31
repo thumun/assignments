2bd4ed60a91698dd62ee9bdc19854b41638449bc
nehathumu <nthumu@haverford.edu>
2022-03-31 15:44:45 -0400

    switching to goldengate

--- A08/mylloc_list.c
+++ A08/mylloc_list.c
@@ -6,6 +6,7 @@ struct chunk {
   int size;
   struct chunk *next;
 };
+
 struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
@@ -13,7 +14,29 @@ void *malloc (size_t size) {
     return NULL;
   }
 
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+
+  // finding a free block
+  // searching flist & if found -> reuse & unlink from flist
+  // else, get more space (new block) from kernel
+  while (next != NULL){
+      if (next -> size >= size){
+          if (prev != NULL){
+              prev->next = next->next;
+          } else {
+              flist = next->next;
+          }
+          return (void*)(next + 1);
+      } else {
+          prev = next;
+          next = next->next;
+      }
+  }
+
+  // sbrk is allocating new memory
   void *memory = sbrk(size + sizeof(struct chunk));
+
   if (memory == (void *) -1) {
     return NULL;
   } else {
@@ -22,10 +45,21 @@ void *malloc (size_t size) {
   }
 }
 
+// I don't get this : (
+// ptr is pointing to same as flist (?)
+// flist updated to be pointer??
 void free(void *memory) {
-  return;
+    if (memory != NULL){
+        // going back one chunk
+        struct chunk *cnk = (struct chunk*)((struct chunk*) memory - 1);
+        cnk -> next = flist;
+        flist = cnk;
+    }
+
+    return;
 }
 
+// figure out what goes here
 void fragstats(void* buffers[], int len) {
 }
 
--- A08/print.c
+++ A08/print.c
@@ -6,5 +6,16 @@
 
 int main (int argc, char* argv[]) {
 
-  return 0 ;
+    void* heapTop = sbrk(0);
+
+    printf("The initial top of the heap is %p", heapTop);
+
+    void* current = sbrk(0);
+
+    printf("The current top of the heap is %p", current);
+
+    // check in bytes!!!
+    printf("Increased by %d (0x%0.3x) bytes", current-heapTop, current-heapTop);
+
+    return 0 ;
 }
