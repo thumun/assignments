4124fc8cfd3cda6359c1ad94e4d3cccf5bd13a05
nehathumu <nthumu@haverford.edu>
2022-04-01 14:28:35 -0400

    .

--- A08/print.c
+++ A08/print.c
@@ -8,8 +8,13 @@ int main (int argc, char* argv[]) {
 
     void* heapTop = sbrk(0);
 
-    printf("hi\n");
-    printf("bye\n");
+    char buff[2000];
+    for (int i = 0; i < 2000; i++){
+        buff[i] = '.';
+    }
+    buff[1999] = '\0';
+
+    printf("%s\n", buff);
     //printf("The initial top of the heap is %p\n", heapTop);
 
     void* current = sbrk(0);
