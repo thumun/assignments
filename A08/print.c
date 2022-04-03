0cf6613826274f152eca2f95e2773a8c6b0bb040
Neha Thumu <71038542+thumun@users.noreply.github.com>
2022-04-02 23:14:04 -0400

    Update print.c

--- A08/print.c
+++ A08/print.c
@@ -8,21 +8,12 @@ int main (int argc, char* argv[]) {
 
     void* heapTop = sbrk(0);
 
-    char buff[2000];
-    for (int i = 0; i < 2000; i++){
-        buff[i] = '.';
-    }
-    buff[1998] = 'n';
-    buff[1999] = '\0';
-
-    printf("%s\n", buff);
-    //printf("The initial top of the heap is %p\n", heapTop);
+    printf("The initial top of the heap is %p\n", heapTop);
 
     void* current = sbrk(0);
 
     printf("The current top of the heap is %p\n", current);
 
-    // check in bytes!!!
     printf("Increased by %ld (0x%lx) bytes\n", current-heapTop, current-heapTop);
 
     return 0 ;
