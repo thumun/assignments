44c5db92440049e824c43a4773d4711877d7b3f1
nehathumu <nthumu@haverford.edu>
2022-05-03 16:12:01 -0400

    test

--- A08/reallocate.c
+++ A08/reallocate.c
@@ -8,6 +8,8 @@ int main (int argc, char* argv[]) {
 
     void* heapTop = sbrk(0);
 
+    printf("The initial top of the heap is %p\n", heapTop);
+
     for (int i = 0; i < 10; i++){
         void * test = (void*) malloc(100);
         free(test);
@@ -16,6 +18,6 @@ int main (int argc, char* argv[]) {
     void* current = sbrk(0);
 
     printf("Bytes allocated %ld (0x%lx) \n", current-heapTop, current-heapTop);
-
+ 
   return 0 ;
 }
